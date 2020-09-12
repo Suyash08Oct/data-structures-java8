fd2586dbef55f13cae39b20f0fd2cfd19943c856
Suyash <up.suyash@gmail.com>
2020-09-12 19:58:25 +0530

    Problems on Stack

---
+++ src/com/problems/ExpressionEvaluation.java
@@ -0,0 +1,61 @@
+package com.problems;
+
+import com.stack.StackLinkedList;
+
+public class ExpressionEvaluation {
+    public static void main(String[] args) {
+        System.out.println("Result: " + evaluate("234*+82/-"));
+    }
+
+    public static int evaluate(String postfix){
+        StackLinkedList stack = new StackLinkedList();
+
+        for(char c : postfix.toCharArray()){
+            int conversion = Character.getNumericValue(c);
+
+            if(isOperation(c)){
+                int r;
+                int y = stack.pop();
+                int x = stack.pop();
+
+                switch (c){
+                    case '+':
+                        r = x + y;
+                        stack.push(r);
+                        break;
+
+                    case '-':
+                        r = x - y;
+                        stack.push(r);
+                        break;
+
+                    case '*':
+                        r = x * y;
+                        stack.push(r);
+                        break;
+
+                    case '/':
+                        r = x / y;
+                        stack.push(r);
+                        break;
+
+                    default:
+                        break;
+
+                }
+
+            } else {
+                stack.push(conversion);
+            }
+        }
+
+        return stack.peek();
+    }
+
+    public static boolean isOperation(char value){
+        if(value == '+' || value == '-' || value == '*' || value == '/')
+            return true;
+        else
+            return false;
+    }
+}
