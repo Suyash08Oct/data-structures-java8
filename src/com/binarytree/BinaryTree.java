3dda91896c6d9364b5184bf0da480cd000e29842
Suyash <up.suyash@gmail.com>
2020-09-18 23:47:35 +0530

    Added Traversal

--- src/com/binarytree/BinaryTree.java
+++ src/com/binarytree/BinaryTree.java
@@ -3,6 +3,7 @@ package com.binarytree;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Scanner;
+import java.util.Stack;
 
 public class BinaryTree{
     public static void main(String[] args){
@@ -38,10 +39,17 @@ public class BinaryTree{
 
         System.out.println("Binary Tree Created Successfully!");
 
+        levelOrder(root);
+        preOrder(root);
+        inOrder(root);
+        postOrder(root);
+    }
+
+    public static void levelOrder(Node temp){
         System.out.println("Level Order Traversal!");
 
         Queue<Node> level = new LinkedList<>();
-        level.add(root);
+        level.add(temp);
 
         while (!level.isEmpty()){
             Node node = level.poll();
@@ -56,5 +64,69 @@ public class BinaryTree{
                 level.add(node.right);
             }
         }
+
+        System.out.println("");
+    }
+
+    public static void preOrder(Node temp){
+        System.out.println("Pre Order Traversal!");
+
+        Stack<Node> preOrder = new Stack<>();
+
+        while(temp != null || !preOrder.isEmpty()){
+            if(temp != null){
+                System.out.print(temp.value + ", ");
+                preOrder.push(temp);
+                temp = temp.left;
+            } else {
+                temp = preOrder.pop();
+                temp = temp.right;
+            }
+        }
+
+        System.out.println("");
+    }
+
+    public static void inOrder(Node current){
+        System.out.println("In Order Traversal!");
+
+        Stack<Node> inOrder = new Stack<>();
+
+        while(current != null || !inOrder.isEmpty()){
+            if(current != null){
+                inOrder.push(current);
+                current = current.left;
+            } else {
+                current = inOrder.pop();
+                System.out.print(current.value + ", ");
+                current = current.right;
+            }
+        }
+
+        System.out.println("");
+    }
+
+    public static void postOrder(Node temp){
+        System.out.println("Post Order Traversal!");
+
+        Stack<Object> postOrder = new Stack<>();
+
+        while (temp != null || !postOrder.isEmpty()){
+            if(temp != null){
+                postOrder.push(temp);
+                temp = temp.left;
+            } else {
+                Object object = postOrder.pop();
+                if(object instanceof Node){
+                    temp = (Node) object;
+                    postOrder.push(temp.value);
+                    temp = temp.right;
+                } else {
+                    System.out.print(object + ", ");
+                }
+            }
+        }
+
+        System.out.println("");
     }
 }
